0063cfab9b6dba993aa6ca4b6dc98f75f590bd53
chandravardhan Kothi <www.kothichandra.com@gmail.com>
2025-01-22 08:20:02 +0530

    from branch

--- swap.py
+++ swap.py
@@ -1,9 +1,10 @@
 def swap(a,b):
-    a = a
-    b = b
+    a = a+b
+    b = b+b
     return a,b
 
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 swap(a,b)
 print(a,b)
+print("i am working from branch directory")
